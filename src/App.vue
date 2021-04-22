277fb26ac640edab223ffa816ab7a670a614cc95
Sunset-Kim <wood2002@naver.com>
2021-04-22 10:03:25 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+import {store} from './store/store.js'
+
+export default {
+  name: 'App',
+  components: {
+  },
+  store,
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
